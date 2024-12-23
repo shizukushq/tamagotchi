48b2f823fe5dc8af7ee57f33938b16788e9efcd3
shizuku <shizukushq@gmail.com>
2024-12-23 14:34:07 +0300

    Added db

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,7 +1,25 @@
-// @ts-nocheck
-import { localExtStorage } from "@webext-core/storage";
-import { onMessage, sendMessage } from "@/utils/messenger";
+import { createClient } from "@supabase/supabase-js";
 
 export default defineBackground(() => {
-  console.info("hello back!");
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_API_URL,
+    import.meta.env.VITE_SUPABASE_API_KEY
+  );
+
+  chrome.runtime.onInstalled.addListener(async (details) => {
+    if (details.reason === "install") {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, "0");
+      const day = String(now.getDate()).padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+
+      const { data, error } = await supabase
+        .from("users_count")
+        .insert([{ first_use: formattedDate }]);
+      if (error) {
+        console.error("Supabase error:", error);
+      }
+    }
+  });
 });
